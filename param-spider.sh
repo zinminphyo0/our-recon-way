9ced918fa299b1753bf8887ed14a8d0befcd544b
zinminphyo0 <74085414+zinminphyo0@users.noreply.github.com>
2023-06-02 05:31:25 +0000

    Add files via upload

---
+++ param-spider.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+subdomains_file=$1
+
+# Get the current directory
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Change to the script directory
+cd "$script_dir"
+
+# Create the output directory if it doesn't exist
+mkdir -p output
+
+# Loop through each subdomain in the file
+while IFS= read -r subdomain; do
+    # Crawl subdomains using ParamSpider
+    python3 "$HOME/ParamSpider/paramspider.py" --domain "$subdomain" --exclude "jpg,jpeg,png,gif,svg,css,ico,pdf,js,woff,ttf,eot,svg,woff2" -o "output/$subdomain-output.txt"
+
+    # Extract parameters from crawled URLs
+    cat "output/$subdomain-output.txt" | grep -oE '(\?|&)([^=]+)=' | cut -d'=' -f1 | sort -u > "output/$subdomain-params.txt"
+done < "$subdomains_file"
